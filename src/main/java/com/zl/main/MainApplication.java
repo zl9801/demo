9ab327fe413d4d1e83d17bc1a4a02bfc60112c25
zl9801 <zhanglie.aptech@163.com>
2020-02-26 10:56:35 +0800

    first

---
+++ src/main/java/com/zl/main/MainApplication.java
@@ -0,0 +1,14 @@
+package com.zl.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
